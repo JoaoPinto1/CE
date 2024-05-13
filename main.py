a264614adb66cacc4cd34431dca28ca6f69e14df
JoaoPinto1 <pintojoao2211@gmail.com>
2024-05-13 17:04:37 +0100

    Final Delivery

--- main.py
+++ main.py
@@ -12,36 +12,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 from itertools import product
 
-def get_data(data1, data2, average1, pop1 , pmut1 , pcross1 , mut1 , cross1, elitism1, average_first_reached, finished):
+def get_data(data1, data2, average1, pop1 , pmut1 , pcross1 , mut1 , cross1, elitism1, average_first_reached, std):
     normal1 = test_normal_ks(data1)
     normal2 = test_normal_ks(data2)
     print(data1)
     print(data2)
     if(normal1 and normal2):
         if(ttest(data1,data2)):
-            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1, finished)
+            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1,std)
             with open("statistics.txt", 'a') as file:
-                file.write("There is a statistically significant difference in average fitness compared to previous runs.\n")
+                file.write("T-test - There is a statistically significant difference in average fitness compared to previous runs.\n")
                 file.write("#######################################################\n\n")
         else:
-            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1, finished)
+            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1,std)
             with open("statistics.txt", 'a') as file:
-                file.write("There is no statistically significant difference in average fitness compared to previous runs.\n")
+                file.write("T-test - There is no statistically significant difference in average fitness compared to previous runs.\n")
                 file.write("#######################################################\n\n")
     else:
-        if(mann_whitney(data1,data2)):
-            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1, finished)
+        if(wilcoxon(data1,data2)):
+            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1,std)
             with open("statistics.txt", 'a') as file:
-                file.write("There is a statistically significant difference in average fitness compared to previous runs.\n")
+                file.write("Wilcoxon - There is a statistically significant difference in average fitness compared to previous runs.\n")
                 file.write("#######################################################\n\n")
         else:
-            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1, finished)
+            write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1 , average_first_reached, elitism1,std)
             with open("statistics.txt", 'a') as file:
-                file.write("There is no statistically significant difference in average fitness compared to previous runs.\n")
+                file.write("Wilcoxon - There is no statistically significant difference in average fitness compared to previous runs.\n")
                 file.write("#######################################################\n\n")
 
 
-def write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1, average_first_reached, elitism1, finished):
+def write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1, average_first_reached, elitism1,std):
     with open("statistics.txt", 'a') as file:
         file.write("#######################################################\n\n")
         file.write("Prob. Crossover: %s\n" % pcross1)
@@ -49,10 +49,9 @@ def write_stats_file(pcross1 , pmut1 , pop1 ,mut1 , cross1 , average1, average_f
         file.write("Population Size: %s\n" % pop1)
         file.write("Mutation: " + mut1.__name__ + "\n")
         file.write("Crossover: " + cross1.__name__ + "\n")
-        file.write("Average obtained: %s\n" % average1)
+        file.write("Average obtained: %s +- %s\n" % (average1, std))
         file.write("Average first reached: %s\n" % average_first_reached)
         file.write("Elitism: %s\n" % elitism1)
-        file.write("Finished: %s\n" % finished)
 
 def test_normal_ks(data):
     """Kolgomorov-Smirnov"""
@@ -66,22 +65,16 @@ def test_normal_ks(data):
         print("Data is not likely normally distributed (reject null hypothesis)")
         return False
 
+        
+def wilcoxon(data1,data2):
+    U_statistic, p_value = stats.wilcoxon(data1,data2)
 
-def mann_whitney(data1, data2):
-    """
-    Non-parametric
-    Two samples
-    Independent
-    """    
-    U_statistic, p_value = stats.mannwhitneyu(data1, data2)
-    
     if p_value < 0.05:
         print("There is a statistically significant difference between the two samples.")
         return True
     else:
         print("There is no statistically significant difference between the two samples.")
         return False
-        
 
 def ttest(previous_fitness , current_fitness):
     
@@ -175,7 +168,6 @@ def teste(config, pop_size, pmut, pcross, mut, cross, elitism):
         config['survivor_selection'] = survivor_elitism(.02, maximization=False)
     else:
         config['survivor_selection'] = survivor_generational
-    finished = False
     print("Testing configuration: ", pop_size, pmut, pcross, mut.__name__, cross.__name__, elitism)
     data2 = []
     for i in range(config['runs']):
@@ -194,8 +186,6 @@ def teste(config, pop_size, pmut, pcross, mut, cross, elitism):
                 best_fitness = value_at_i
                 first_max_index = a
         
-        if best_fitness == 22:
-            finished = True
         fitness_reached.append(first_max_index)
         average_first_reached = sum(fitness_reached) / len(fitness_reached)
 
@@ -203,16 +193,17 @@ def teste(config, pop_size, pmut, pcross, mut, cross, elitism):
         print("Run: ", i)
 
     average_fitness = sum(data2) / len(data2)
+    std = np.std(data2)
     config['seed'] = 1234
-    print("Average fitness: ", average_fitness)
-    return [data2, average_fitness, pop_size, pmut, pcross, mut, cross, elitism, average_first_reached, finished]
+    print("Average fitness: ", average_fitness, "Standard deviation: ", std)
+    return [data2, average_fitness, pop_size, pmut, pcross, mut, cross, elitism, average_first_reached, std]
 
 
 if __name__ == '__main__':
     # Dictonary with Configurations for the Evolutionary Algorithm
     config = {
         'map_size' : 12,
-        'runs' : 30,
+        'runs' : 1,
         'population_size' : 150,
         'generations' : 50,
         'prob_crossover' : 0.8,
@@ -222,11 +213,11 @@ if __name__ == '__main__':
         'mapping' : mapping,
         'maximization' : False,
         'mutation' : main_mutation, #TODO: implement mutation function,
-        'crossover' : sample_crossover, #TODO: implement crossover function,
+        'crossover' : one_point_crossover, #TODO: implement crossover function,
         'parent_selection' : tournament(5, maximization=False),
         'survivor_selection' : survivor_elitism(.02, maximization=False),
         'fitness_function' : None,
-        'interactive_plot' : create_interactive_plot('Evolving...', 'Iteration', 'Quality', (0, 2000), (-2, 10)),
+        'interactive_plot' : create_interactive_plot('Evolving...', 'Iteration', 'Quality', (0, 50), (10, 200)),
     }
     
     if config['map_size'] == 4:
@@ -240,13 +231,6 @@ if __name__ == '__main__':
     
     config['fitness_function'] = function_fitness(config)
     
-    population_size = [150 , 100 , 50]
-    prob_mutation = [0.05 , 0.1, 0.15]
-    prob_crossover = [0.7, 0.8, 0.9]
-    mutations = [main_mutation , change_value_mutation]
-    crossovers = [sample_crossover, one_point_crossover, two_point_crossover]
-    elitism = [True , False]
-
     data1 = []
     fitness_reached = []
     for i in range(config['runs']):
@@ -266,25 +250,30 @@ if __name__ == '__main__':
                 first_max_index = a
         
         fitness_reached.append(first_max_index)
-        average_first_reached = sum(fitness_reached) / len(fitness_reached)
 
         data1.append(total_fitness / config['generations'])
         print("Run: ", i)
     config['seed'] = 1234
-    average1 = sum(data1) / len(data1)
+    average1 = np.mean(data1)
+    std = np.std(data1)
+    average_first_reached = np.mean(fitness_reached)
 
-    print("Average fitness: ", average1)
-
+    print("Average fitness: ", average1, "Standard deviation: ", std, "Average first reached: ", average_first_reached)
+    write_stats_file(0.8 , 0.05 , 150 ,main_mutation , one_point_crossover , average1, average_first_reached, True, std)
+    
     testes = []
-    testes.append(teste(config, 50, 0.05, 0.8, main_mutation, sample_crossover, True))
-    testes.append(teste(config, 150, 0.05, 0.8, delete_mutation, sample_crossover, True))
-    testes.append(teste(config, 150, 0.1, 0.8, main_mutation, sample_crossover, True))
-    testes.append(teste(config, 150, 0.05, 0.9, main_mutation, sample_crossover, True))
-    testes.append(teste(config, 150, 0.05, 0.8, main_mutation, one_point_crossover, True))
+    testes.append(teste(config, 50, 0.05, 0.8, main_mutation, one_point_crossover, True))
+    testes.append(teste(config, 150, 0.05, 0.8, delete_mutation, one_point_crossover, True))
+    testes.append(teste(config, 150, 0.1, 0.8, main_mutation, one_point_crossover, True))
+    testes.append(teste(config, 150, 0.05, 0.9, main_mutation, one_point_crossover, True))
+    testes.append(teste(config, 150, 0.05, 0.7, main_mutation, one_point_crossover, True))
     testes.append(teste(config, 150, 0.05, 0.8, main_mutation, two_point_crossover, True))
-    testes.append(teste(config, 150, 0.05, 0.8, main_mutation, sample_crossover, False))
-    testes.append(teste(config, 100, 0.05, 0.8, main_mutation, sample_crossover, False))
-    testes.append(teste(config, 150, 0.05, 0.8, insert_mutation, sample_crossover, True))
+    testes.append(teste(config, 150, 0.05, 0.8, main_mutation, two_point_crossover, False))
+    testes.append(teste(config, 150, 0.05, 0.8, main_mutation, one_point_crossover, False))
+    testes.append(teste(config, 100, 0.05, 0.8, main_mutation, one_point_crossover, False))
+    testes.append(teste(config, 150, 0.05, 0.8, insert_mutation, one_point_crossover, True))
+    testes.append(teste(config, 150, 0.05, 0.8, change_value_mutation, one_point_crossover, True))
+    testes.append(teste(config, 150, 0.2, 0.8, main_mutation, one_point_crossover, True))
 
     for test in testes:
         get_data(data1, test[0], test[1], test[2], test[3], test[4], test[5], test[6], test[7], test[8], test[9])
--- variation_operators.py
+++ variation_operators.py
@@ -3,43 +3,13 @@ import random
 
 ## Crossovers ##
 
-## Binary ##
-
-
-def no_crossover(parent1, parent2):
-    return parent1
-
-def one_point_crossover(parent1, parent2):
-    at = random.randint(1, len(parent1['genotype'])-1)
-    if at >= len(parent2['genotype'])-1:
-        at = len(parent2['genotype']) - 1
-    genotype = parent1['genotype'][:at] + parent2['genotype'][at:]
-
-    return {'genotype': genotype, 'fitness': None} 
-
-
 def two_point_crossover(parent1, parent2):
-    cut_points = sorted(random.sample(range(len(parent1['genotype'])), 2))
-    genotype = parent1['genotype'][ 0 : cut_points[0] ] +  parent2['genotype'][ cut_points[0] : cut_points[1]] + parent1['genotype'][ cut_points[1] : ] 
+    cut_points1 = sorted(random.sample(range(len(parent1['genotype'])), 2))
+    cut_points2 = sorted(random.sample(range(len(parent2['genotype'])), 2))
+    genotype = parent1['genotype'][: cut_points1[0] ] +  parent2['genotype'][ cut_points2[0] : cut_points2[1]] + parent1['genotype'][ cut_points1[1] : ]
     return {'genotype': genotype, 'fitness': None}
 
-
-def uniform_crossover(parent1, parent2):
-    """
-    Uniform Crossover
-    """
-    mask = [random.random() for i in range(len(parent1['genotype']))]
-    genotype = []
-    for i in range(len(parent1['genotype'])):
-        if mask[i] < 0.5:
-            genotype.append(parent1['genotype'][i])
-        else:
-            genotype.append(parent2['genotype'][i])
-    return {'genotype': genotype, 'fitness': None} 
-
-
-## Integers ##
-def sample_crossover(parent1, parent2):
+def one_point_crossover(parent1, parent2):
     size_1 = len(parent1['genotype'])
     size_2 = len(parent2['genotype'])    
     pos_1 = random.randint(1, size_1 - 1)
@@ -47,71 +17,9 @@ def sample_crossover(parent1, parent2):
     genotype = parent1['genotype'][:pos_1] + parent2['genotype'][pos_2:]
     return {'genotype': genotype, 'fitness': None}
 
-## Float ##
-# Variation Operators : Aritmetical  Crossover
-def aritmetical_cross(alpha):
-    def cross(p1, p2):
-        size = len(p1['genotype'])
-        genotype = [None] * size
-        at = random.randint(0, len(p1['genotype']) - 1)
-        genotype[ : at] = p1['genotype'][ : at]
-        for i in range(at, size):
-            genotype[i] = alpha * p1['genotype'][i] + (1 - alpha) * p2['genotype'][i]
-        return {'genotype': genotype, 'fitness': None}
-    return cross
-
-
-def heuristic_cross(alpha, domain):
-    def cross(p1, p2):
-        size = len(p1['genotype'])
-        genotype = []
-        # TODO: YOUR CODE HERE
-        for i in range(size):
-            if random.random() < alpha:
-                genotype.append(p1['genotype'][i])
-            else:
-                genotype.append(p2['genotype'][i])
-        return {'genotype': genotype, 'fitness': None}
-    
-    return cross
-
-## Permutations ##
-def order_crossover(p1, p2):
-    size = len(p1['genotype'])
-    # Select a random subsequence from parent1
-    start, end = sorted(random.sample(range(size), 2))
-    
-    # Copy the subsequence to the offspring
-    genotype = [None] * size
-    genotype[start:end+1] = p1['genotype'][start : end+1]
-    
-    # Fill the remaining positions with elements from parent2
-    # Skip the elements already present in the offspring
-    p2_index = (end + 1) % size  # Start from the end of the selected subsequence in parent2
-    for i in range(size - (end - start + 1)):
-        while p2['genotype'][p2_index] in genotype:
-            p2_index = (p2_index + 1) % size
-        genotype[(end + 1 + i) % size] = p2['genotype'][p2_index]
-    return {'genotype': genotype, 'fitness': None}
-
-
-def cycle_crossover(p1, p2):
-    size = len(p1['genotype'])
-    genotype = []
-   # TODO: YOUR CODE HERE
-    
-    return {'genotype': genotype, 'fitness': None}
 
 ## Mutations ##
-## Binary ##
-def no_mutation(p):
-    return p
 
-def bit_flip_mutation(p):
-    p['fitness'] = None
-    at = random.randint(0, len(p['genotype']) - 1)
-    p['genotype'][at] ^= 1 
-    return p
 
 def delete_mutation(p):
     p['fitness'] = None
@@ -144,71 +52,5 @@ def main_mutation(p):
     return p
     
 
-## Integers ##
-def append_mutation(genotype_size):
-    def mutation(p):
-        p['fitness'] = None
-        universe = set(list(range(1, genotype_size + 1)))
-        elements = set(p['genotype'])
-        candidates = list(universe.difference(elements))
-        element = random.choice(candidates)
-        p['genotype'].append(element)
-        return p
-    return mutation
-
-
-def changing_value_mutation(neighborhood_size, genotype_size):
-    def mutation(p):
-        p['fitness'] = None
-        index = random.choice(list(range(len(p['genotype']))))
-        number = p['genotype'].pop(index)
-        delta = random.choice(list(range(-neighborhood_size,neighborhood_size)))
-        number = (number + delta)
-        if number < 1:
-            number =  1 
-        if number > genotype_size:
-            number = genotype_size 
-        if number not in p['genotype']:
-            p['genotype'].insert(index,number)
-        return p
-    return mutation
-
-## Floats ##
-
-def gaussian_mutation(sigma, domain):
-    def mutation(p):
-        p['fitness'] = None
-        at = random.randint(0, len(p['genotype']) - 1)
-        gene = random.gauss(p['genotype'][at], sigma)
-        p['genotype'][at] = max(domain[0], min(gene, domain[1])) #clip to domain
-        return p
-    return mutation
-
-def uniform_mutation(domain):
-    def mutation(p):
-        p['fitness'] = None
-        at = random.randint(0, len(p['genotype']) - 1)
-        p['genotype'][at] = random.uniform(domain[0], domain[1])
-        return p
-    return mutation
-
-## Permutations ##
-
-def swap_mutation(p):
-    p['fitness'] = None
-    index1 = random.randrange(len(p['genotype']))
-    index2 = index1
-    while index2 == index1:
-        index2 = random.randrange(len(p['genotype']))
-    p['genotype'][index1], p['genotype'][index2] = p['genotype'][index2], p['genotype'][index1]
-    return p
-
-
-
-def scramble_mutation(p):
-    p['fitness'] = None
-    # TODO: YOUR CODE HERE
-    
-    return p
 
 
